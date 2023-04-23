519fa80b38fd5da7932e924f0a94e54a630b57db
Abhinava Sanyal <abhinav.sanyal@sleek.com>
2023-04-23 08:56:44 +0530

    deploy changes

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://athenaapi:3002/api/auth/google/callback",
+        callbackURL: "https://aggressive-hen-tie.cyclic.app/api/auth/google/callback",
         passReqToCallback   : true
       },
       async (accessToken, refreshToken, profile, done) => {
